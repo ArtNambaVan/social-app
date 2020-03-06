c263e8032f07d9c57efdef797c7a20f5ad8819d2
Artem Klochkov <artem.klochkov@soft-werke.com>
2020-03-06 17:52:13 +0300

    init socialApp

---
+++ socialApp-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-03-02T12:22:39.878Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
